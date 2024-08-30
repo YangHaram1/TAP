9ad58c0ef9999b06557c1e6aa9a018ba11303089
YANGHARAM <apdlvmf33@naver.com>
2024-08-30 14:11:41 +0900

    setting

--- TAP-BackEnd/src/main/java/com/tap/controllers/MembersController.java
+++ TAP-BackEnd/src/main/java/com/tap/controllers/MembersController.java
@@ -1,5 +1,9 @@
 package com.tap.controllers;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,4 +11,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/members")
 public class MembersController {
 	
+	@GetMapping
+	public ResponseEntity<String> getName(@AuthenticationPrincipal UserDetails user){
+		
+		return ResponseEntity.ok(user.getUsername());
+	}
 }
