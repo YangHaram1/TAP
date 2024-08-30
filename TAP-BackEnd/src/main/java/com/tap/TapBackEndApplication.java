1624963173d88f4cee3f501a132f173be43538cd
YANGHARAM <apdlvmf33@naver.com>
2024-08-30 13:39:36 +0900

    setting

---
+++ TAP-BackEnd/src/main/java/com/tap/TapBackEndApplication.java
@@ -0,0 +1,13 @@
+package com.tap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TapBackEndApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TapBackEndApplication.class, args);
+	}
+
+}
---
+++ TAP-BackEnd/src/test/java/com/tap/TapBackEndApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TapBackEndApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
