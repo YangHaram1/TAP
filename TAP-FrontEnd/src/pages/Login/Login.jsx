44c6e987d6226d09b3a97a174edd41d4aaa279fb
YANGHARAM <apdlvmf33@naver.com>
2024-08-30 15:45:04 +0900

    setting

--- TAP-FrontEnd/src/pages/Login/Login.jsx
+++ TAP-FrontEnd/src/pages/Login/Login.jsx
@@ -1,12 +1,29 @@
 import styles from './Login.module.css';
 import { useNavigate } from "react-router-dom";
+import App from './../../App';
 
-const Login =()=>{
+const Login = () => {
     const navi = useNavigate();
 
-    return(
+    return (
         <div className={styles.container}>
-            로그인
+            <div className={styles.title}>
+
+            </div>
+            <div className={styles.form}>
+                <div>
+                    아이디
+                </div>
+                <div>
+                    비번
+                </div>
+                <div>
+                로그인 상태
+            </div>
+            </div>
+            <div>
+                <button className={styles.button}>로그인 </button>
+            </div>
         </div>
     );
 
