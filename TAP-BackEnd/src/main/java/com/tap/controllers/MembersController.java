3ab93cfb460eb86f0a689c5f7cd5e575dfe4a740
YANGHARAM <apdlvmf33@naver.com>
2024-08-30 14:03:08 +0900

    update

---
+++ TAP-BackEnd/src/main/java/com/tap/controllers/MembersController.java
@@ -0,0 +1,10 @@
+package com.tap.controllers;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/members")
+public class MembersController {
+	
+}
