880bfec47f1495195508d531a373af8f5ff0628b
Hayun Jeong <1116jjee@gmail.com>
2024-09-04 17:13:00 +0900

    EventApply.jsx 입력폼만 작성

--- TAP-FrontEnd/src/pages/ABusiness/ABuizContent.jsx
+++ TAP-FrontEnd/src/pages/ABusiness/ABuizContent.jsx
@@ -1,17 +1,17 @@
 import { EventApply } from "./EventApply/EventApply";
 import styles from './ABuizContent.module.css';
-import { Route, Routes } from "react-router-dom";
+import { Route, Router, Routes } from "react-router-dom";
 import { BuizSideMenu } from "../ASideMenu/BuizSideMenu";
 
 export const ABuizContent=()=>{
 
     return(
         <div className={styles.container}>
-            <BuizSideMenu/>
+          
             <Routes>
-                <Route path="/applyevent/*" element={<EventApply/>}/>
+            <Route path="/applyevent/*" element={<EventApply/>}/>
             </Routes>
-        </div>
+            </div>
     );
       
 }
--- TAP-FrontEnd/src/pages/ASideMenu/BuizSideMenu.jsx
+++ TAP-FrontEnd/src/pages/ASideMenu/BuizSideMenu.jsx
@@ -1,3 +1,10 @@
+import styles from './AdminSideMenu.module.css'
+
 export const BuizSideMenu =()=>{
-    
+
+    return(
+        <div className={styles.container}>
+            상품등록내역
+        </div>
+    )
 }
