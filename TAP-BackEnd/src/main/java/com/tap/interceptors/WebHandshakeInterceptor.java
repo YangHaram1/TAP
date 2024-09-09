36c11009add194f6d441235ad5d86bf657a51614
YANGHARAM <apdlvmf33@naver.com>
2024-09-09 20:31:23 +0900

    security 수정 및 websocket interceptor 추가 및 웹소켓 토큰 검증 완료

---
+++ TAP-BackEnd/src/main/java/com/tap/interceptors/WebHandshakeInterceptor.java
@@ -0,0 +1,29 @@
+package com.tap.interceptors;
+
+
+import java.util.Map;
+
+import org.springframework.http.server.ServerHttpRequest;
+import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.security.core.context.SecurityContext;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.socket.WebSocketHandler;
+import org.springframework.web.socket.server.HandshakeInterceptor;
+
+public class WebHandshakeInterceptor implements HandshakeInterceptor {
+	@Override
+	public boolean beforeHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+			Map<String, Object> attributes) throws Exception {
+		 SecurityContext securityContext = SecurityContextHolder.getContext();
+	        if (securityContext != null) {
+	            attributes.put("SPRING_SECURITY_CONTEXT", securityContext);
+	        }
+
+		return true;
+	}
+
+	@Override
+	public void afterHandshake(ServerHttpRequest request, ServerHttpResponse response, WebSocketHandler wsHandler,
+			Exception ex) {
+	}
+}
