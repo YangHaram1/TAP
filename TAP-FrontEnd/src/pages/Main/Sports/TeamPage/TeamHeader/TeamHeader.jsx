d01371b3d1e9888770cb12104043ab79d56a2531
SSH <kps10011@naver.com>
2024-09-10 10:48:30 +0900

    구단 경기 정보 페이징

--- TAP-FrontEnd/src/pages/Main/Sports/TeamPage/TeamHeader/TeamHeader.jsx
+++ TAP-FrontEnd/src/pages/Main/Sports/TeamPage/TeamHeader/TeamHeader.jsx
@@ -1,13 +1,16 @@
-// src/pages/TeamPage/TeamHeader.jsx
 import React from 'react';
-import styles from './TeamHeader.module.css'
+import styles from './TeamHeader.module.css';
 
 export const TeamHeader = ({ teamName, teamLogo, homeGround }) => {
   return (
     <div className={styles.teamHeader}>
-      <img src={teamLogo} alt={`${teamName} 로고`} className={styles.teamLogo} />
-      <h1 className={styles.teamName}>{teamName}</h1>
-      <p className={styles.homeGround}>홈구장: {homeGround}</p>
+      <div className={styles.logoContainer}>
+        <img src={teamLogo} alt={`${teamName} 로고`} className={styles.teamLogo} />
+      </div>
+      <div className={styles.infoContainer}>
+        <h1 className={styles.teamName}>{teamName}</h1>
+        <p className={styles.homeGround}>홈구장: {homeGround}</p>
+      </div>
     </div>
   );
 };
