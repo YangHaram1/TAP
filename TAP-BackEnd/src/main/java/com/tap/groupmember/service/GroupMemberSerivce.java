2da892dc2c5c0709bf9783a350d97f527ba829cf
YANGHARAM <apdlvmf33@naver.com>
2024-09-10 14:04:38 +0900

    채팅 기능 추가

--- TAP-BackEnd/src/main/java/com/tap/groupmember/service/GroupMemberSerivce.java
+++ TAP-BackEnd/src/main/java/com/tap/groupmember/service/GroupMemberSerivce.java
@@ -3,13 +3,22 @@ package com.tap.groupmember.service;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.tap.groupmember.dao.GroupMemberDAO;
 import com.tap.groupmember.dto.GroupMemberDTO;
 
 @Service
 public class GroupMemberSerivce {
-
+	
+	@Autowired
+	private GroupMemberDAO dao;
+	
+	public void insert(int seq,String id) throws Exception{
+		dao.insert(seq, id);
+	}
+	
 	
 	public List<GroupMemberDTO> members(int groupSeq){
 		List<GroupMemberDTO> list=new ArrayList<>();
--- TAP-FrontEnd/src/pages/Chat/ChatApp/ChatApp.jsx
+++ TAP-FrontEnd/src/pages/Chat/ChatApp/ChatApp.jsx
@@ -1,22 +1,28 @@
 import styles from './ChatApp.module.css';
 import { useAuthStore, useCheckList, useNotification } from '../../../store/store';
-import { useContext, useEffect, useState, useCallback, useRef } from 'react';
+import React, { useContext, useEffect, useState, useCallback, useRef } from 'react';
 import { ChatsContext } from '../../../context/ChatsContext';
-import { api } from './../../../config/config';
 import { toast } from 'react-toastify';
+import { format } from 'date-fns';
+import SweetAlert from '../../../components/SweetAlert/SweetAlert';
+import { host, api } from './../../../config/config';
+import avatar from '../../../images/ai.png'
 const ChatApp = () => {
+
+    let lastDate = null // 이거 날짜 체크할떄 
+
     const editorRef = useRef(null);
-    const [chats, setChats] = useState();
+    const [chats, setChats] = useState([]);
+    const [list, setList] = useState();
 
     const { isAuth, loginID } = useAuthStore();
-    const { ws, setChatNavi, chatNavi, dragRef,chatAppRef } = useContext(ChatsContext);
-    const {  chatController,chatSeq,onMessage, setChatSeq, setChatController ,} = useCheckList();
+    const { ws, setChatNavi, chatNavi, dragRef, chatAppRef } = useContext(ChatsContext);
+    const { chatController, chatSeq, onMessage, setChatSeq, setChatController, } = useCheckList();
 
 
     // WebSocket 연결을 설정하는 useEffect
     useEffect(() => {
-
-        if (isAuth &&ws.current!=null) {    
+        if (isAuth && ws.current != null) {
             console.log("chatapp")
             ws.current.onmessage = (e) => {
                 if (e.data === 'chatController') {
@@ -45,18 +51,19 @@ const ChatApp = () => {
                     }
                 }
             }
-
         }
     }, [onMessage]);//chatNavi, 이거뻇음 일단
 
 
     useEffect(() => {
         if (isAuth) {
-            const { chatSeq } = useCheckList.getState();
-            if (chatSeq !== 0) {
-                api.get(`/chat?chatSeq=${chatSeq}`).then(resp => {//채팅목록 가저오기
-                    setChats(resp.data);
-                })
+            if (chatNavi === 'chatapp') {
+                const { chatSeq } = useCheckList.getState();
+                if (chatSeq !== 0) {
+                    api.get(`/chat?chatSeq=${chatSeq}`).then(resp => {//채팅목록 가저오기
+                        setChats(resp.data);
+                    })
+                }
             }
         }
     }, [chatNavi, chatController])
@@ -95,9 +102,94 @@ const ChatApp = () => {
         });
     }
 
+
+    //다운로드 컨트롤
+    const handleDownload = (split) => {
+        const linkElement = document.createElement('a');
+        // 2. 링크 속성 설정
+        linkElement.href = `${host}/files/downloadChat?oriname=${split[0]}&sysname=${split[1]}`;
+        linkElement.download = split[0];
+        linkElement.click();
+    }
+
+    //채팅 list 목록 출력
+    const handleChatsData = useCallback(() => {
+        let count = 0;
+
+        setList(
+            chats.map((item, index) => {
+                //---------------------------------------------// 날짜 로직 
+                const formattedTimestamp = format(new Date(item.write_date), 'a hh:mm').replace('AM', '오전').replace('PM', '오후');
+                const currentDate = format(new Date(item.write_date), 'yyyy-MM-dd');
+                const isDateChanged = currentDate !== lastDate;
+                if (isDateChanged) {
+                    lastDate = currentDate;
+                }
+
+                //--------------------------------------------------// 내가쓴글인지 아닌지
+                let idCheck = false;
+                if (item.member_id === loginID) {
+                    idCheck = true;
+                }
+                //--------------------------------------------------// 여기가 파일쪽 로직 처리
+                let fileCheck = false;
+                let file = '';
+                if (item.upload_seq !== 0) {
+                    const split = item.message.split('*');
+                    fileCheck = true;
+                    if (split[2] === '2') {
+                        file = `<p style="color: blue; cursor: pointer;">${split[0]}</p>`;
+                    }
+                    else if (split[2] === '1') {
+                        file = `<p style='color: blue; cursor: pointer;'><img src='${host}/images/chat/${split[1]}' alt=downloadImage></img></p>`;
+                    }
+                }
+                //--------------------------------------------------// 여긴 시스템 로직 처리
+                let systemCheck = false;
+                if (item.member_id === 'system') {
+                    systemCheck = true;
+                }
+                //--------------------------------------------------//
+                return (
+                    <React.Fragment key={index}>
+                        {isDateChanged && (
+                            <div className={styles.dateSeparator}>{currentDate}</div>
+                        )}
+                        {systemCheck && (
+                            <div className={styles.system}><p>{item.message}</p></div>
+                        )}
+                        {!systemCheck && (
+                            <div className={idCheck ? styles.div1Left : styles.div1} >
+                                {
+                                    !idCheck && (<div className={styles.avatar}><img src={avatar} alt="" /></div>)
+                                }
+                                <div>
+                                    <div className={idCheck ? styles.nameReverse : styles.name}>{item.name}</div>
+                                    <div className={idCheck ? styles.contentReverse : styles.content}>
+                                        <div dangerouslySetInnerHTML={{ __html: ((fileCheck ? file : item.message)) }}
+                                            className={idCheck ? styles.mboxReverse : styles.mbox} onClick={fileCheck ? () => SweetAlert('warning', '채팅방', '다운로드를 진행하시겠습니까?', () => handleDownload(item.message.split('*'))) : undefined}></div>
+                                        <div style={{ display: "flex" }}>
+                                            <div className={styles.date}>{formattedTimestamp}</div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>)}
+                    </React.Fragment>
+                );
+            })
+        );
+
+    }, [chats])
+
+    useEffect(() => {
+        handleChatsData();
+    }, [handleChatsData])
+
     return (
         <div className={styles.container} ref={chatAppRef}>
-            dada
+            {
+                list
+            }
         </div>
     )
 }
