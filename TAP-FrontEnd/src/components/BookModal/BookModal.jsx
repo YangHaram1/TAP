cb0299d8d75573496a0de93ad09e90e7e3d7ca37
kjy <kimjy2718@gmail.com>
2024-09-12 12:06:34 +0900

    price page create

--- TAP-FrontEnd/src/components/BookModal/BookModal.jsx
+++ TAP-FrontEnd/src/components/BookModal/BookModal.jsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react';
 import styles from './BookModal.module.css';
 import { PreventMacro } from './PreventMacro/PreventMacro';
 import { api } from '../../config/config';
+import { PriceModal } from '../PriceModal/PriceModal';
 
 export const BookModal = ({ isOpen, onClose }) =>{
 
@@ -14,7 +15,7 @@ export const BookModal = ({ isOpen, onClose }) =>{
     const [seats, setSeats] = useState({row:0,col:0}); 
     const [section, setSection] = useState("");
     const [selectedSeats, setSelectedSeats] = useState([]); // 선택된 좌석 상태
-
+    const [priceModal, setPriceModal] = useState(false);
     const [seatsLevel, setSeatsLevel] = useState([{level:"VIP", price:150000}, {level:"R", price:130000}, {level:"S", price:110000}, {level:"A", price:90000}])
 
     // seq => 상품 번호
@@ -33,20 +34,22 @@ export const BookModal = ({ isOpen, onClose }) =>{
     // })
     // 장소 (총좌석수, 좌석 모양) -> 
 
-    // 매크로 모달 활성화 코드 // 예매 코드 완료 후 주석 풀 예정
-    const isMacroModalOpen = ()=>{
-        setMacroModal(true);
-    }
 
-    const closeMacroModal=()=>{
-        setMacroModal(false);
+    //======================== 매크로 모달 활성화 ======================
+    // 매크로 모달 활성화 코드 // 예매 코드 완료 후 주석 풀 예정
+    const isMacroModalOpen = ()=>{setMacroModal(true);}
+
+    const closeMacroModal=()=>{setMacroModal(false);
     }
 
     // BookModal 모달을 닫을 때, PreventMacro 모달도 초기화
     const handleCloseBookModal = () => {
         setMacroModal(true);   // PreventMacro 모달을 다시 열리도록 상태 초기화
         onClose();             // 부모로부터 받은 onClose로 BookModal 닫음
+        console.log("전체 페이지 닫히는 중",macroModal);
     }
+
+    //======================== 좌석 세팅 ======================
 
     // 우측 상단 섹션(층수) 선택 시 좌측 세부 좌석이 바뀜
     const handleSection =(section)=>{
@@ -90,7 +93,7 @@ export const BookModal = ({ isOpen, onClose }) =>{
 
     useEffect(()=>{
         setSelectedSeats([]);
-    },[section]);
+    },[section, macroModal]);
 
     const getBackgroundColor = (level) => {
         switch (level) {
@@ -109,146 +112,152 @@ export const BookModal = ({ isOpen, onClose }) =>{
 
     if (!isOpen) return null;
 
+    //======================== 다음 페이지() 세팅 ======================
+
+    const handlePriceModal = ()=>{
+        setPriceModal(true);
+        // onClose();
+    }
+
+    const handleClosePriceModal = ()=>{
+        setPriceModal(false);
+    }
+
     return(
         <div className={styles.overlay}>
             {/* PreventMacro 모달이 열려있을 때만 렌더링 */}
-            {/* {macroModal && <PreventMacro onClose={closeMacroModal} />} */}
+            {macroModal && <PreventMacro onClose={closeMacroModal} />}
             
-            {/* {!macroModal && (
+            {!macroModal && (
                 <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-                    <div className={styles.header}>
-                        <div className={styles.header_title}></div>
+                <div className={styles.header}>
+                    <div className={styles.header_title}>
+                        STEP 2
+                    </div>
+                    <div className={styles.header_middle}>
+                        <p><span style={{fontSize:"18px", fontWeight:"700"}}> 뮤지컬 &lt;킹키부츠&gt;</span><span style={{color:"lightgray", fontSize:"12px"}}>&nbsp;&nbsp;| 블루스퀘어홀</span></p>
+                        <div className={styles.header_middle_select}>
+                            <p>선택한 날짜</p> &nbsp;
+                            <select>
+                                <option>2024년 09월 10일(수)</option>
+                                <option>2024년 09월 11일(목)</option>
+                                <option>2024년 09월 12일(금)</option>
+                            </select>&nbsp;
+                            <select>
+                                <option>12시 00분</option>
+                                <option>19시 00분</option>
+                            </select>
+
+                        </div>
+                    </div>
+                    <div className={styles.header_end}>
                         <button className={styles.closeButton} onClick={handleCloseBookModal}>×</button>
                     </div>
-                    모달창 띄우기
                 </div>
-            )} */}
 
-                {/* ui 구성하는 동안은 매크로 방지 제거 */}
-                <div className={styles.modal} onClick={(e) => e.stopPropagation()}>
-                    <div className={styles.header}>
-                        <div className={styles.header_title}>
-                            STEP 1
-                        </div>
-                        <div className={styles.header_middle}>
-                            <p><span style={{fontSize:"18px", fontWeight:"700"}}> 뮤지컬 &lt;킹키부츠&gt;</span><span style={{color:"lightgray", fontSize:"12px"}}>&nbsp;&nbsp;| 블루스퀘어홀</span></p>
-                            <div className={styles.header_middle_select}>
-                                <p>선택한 날짜</p> &nbsp;
-                                <select>
-                                    <option>2024년 09월 10일(수)</option>
-                                    <option>2024년 09월 11일(목)</option>
-                                    <option>2024년 09월 12일(금)</option>
-                                </select>&nbsp;
-                                <select>
-                                    <option>12시 00분</option>
-                                    <option>19시 00분</option>
-                                </select>
+                <div className={styles.main}>
 
-                            </div>
-                        </div>
-                        <div className={styles.header_end}>
-                            <button className={styles.closeButton} onClick={handleCloseBookModal}>×</button>
-                        </div>
-                    </div>
+                    {/* 여기 컴포넌트를 갈아 끼워야 됨 */}
 
-                    <div className={styles.main}>
+                    <div className={styles.main_left}>
+                        {(() => {
+                            let buttons = [];
+                            for (let i = 0; i < seats.row; i++) {
+                                let rowButtons = [];
+                                for (let j = 0; j < seats.col; j++) {
+                                    const seatId = `${i}-${j}`; // 좌석 ID
+                                    const isSelected = selectedSeats.includes(seatId); // 좌석 선택 여부
 
-                        {/* 여기 컴포넌트를 갈아 끼워야 됨 */}
-
-                        <div className={styles.main_left}>
-                            {(() => {
-                                let buttons = [];
-                                for (let i = 0; i < seats.row; i++) {
-                                    let rowButtons = [];
-                                    for (let j = 0; j < seats.col; j++) {
-                                        const seatId = `${i}-${j}`; // 좌석 ID
-                                        const isSelected = selectedSeats.includes(seatId); // 좌석 선택 여부
-
-                                        rowButtons.push(
-                                            <button
-                                                key={seatId}
-                                                className={styles.seat_btn}
-                                                style={{ backgroundColor: isSelected ? 'red' : 'lightgray' }} // 선택된 좌석만 빨간색
-                                                onClick={() => toggleSeatSelection(i, j)}
-                                            >
-                                                {j + 1}
-                                            </button>
-                                        );
-                                    }
-                                    buttons.push(
-                                        <div key={`row-${i}`} className={styles.row}>
-                                            <p>{i + 1}행</p>
-                                            {rowButtons}
-                                        </div>
+                                    rowButtons.push(
+                                        <button
+                                            key={seatId}
+                                            className={styles.seat_btn}
+                                            style={{ backgroundColor: isSelected ? 'red' : 'lightgray' }} // 선택된 좌석만 빨간색
+                                            onClick={() => toggleSeatSelection(i, j)}
+                                        >
+                                            {j + 1}
+                                        </button>
                                     );
                                 }
-                                return buttons;
-                            })()}
+                                buttons.push(
+                                    <div key={`row-${i}`} className={styles.row}>
+                                        <p>{i + 1}행</p>
+                                        {rowButtons}
+                                    </div>
+                                );
+                            }
+                            return buttons;
+                        })()}
+                    </div>
+                    <div className={styles.main_right}>
+                        <div className={styles.main_right_header}>
+                            원하시는 좌석을 선택해주세요
                         </div>
-                        <div className={styles.main_right}>
-                            <div className={styles.main_right_header}>
-                                원하시는 좌석을 선택해주세요
+                        <div className={styles.main_right_section}>
+                            <div className={styles.main_right_section_img}>
+                            {
+                                sections.map((section, i)=>{
+                                    return(<button key={i} className={styles.section_btns} onClick={()=>{handleSection(section)}}>{section}</button>);
+                                })
+                           }
                             </div>
-                            <div className={styles.main_right_section}>
-                                <div className={styles.main_right_section_img}>
+                        </div>
+                        <div className={styles.seats_level}>
+                            <div className={styles.seats_level_text}> 좌석 등급 / 잔여석 </div>
+                            
+                            <div className={styles.seats_level_box}>
+                            {seatsLevel.map((level, i) => (
+                                <div key={i} className={styles.level_box_data}>
+                                    <div className={styles.seats_level_color} style={{ backgroundColor: getBackgroundColor(level.level) }}></div>
+                                    {level.level}석 {level.price.toLocaleString()}원
+                                </div>
+                            ))}
+                            </div>
+                            {/* 등급에 대해서는 고민필요
+                                1. 만약 열에 대해 등급을 고정시킬 경우 (ex> 상영관 상관없이 1층 10행 열전체는 vip, 1층 나머지는 r)
+                                   db를 수정할 필요없이 랜더링 시 조건 부여해서 출력하면 될것같음. 
+                                2. db에 등급을 넣을 경우 확실히 확장성이 늘어남, 근데 어떻게 db에 넣어야할지 감이 안잡힌다. 
+                            */}    
+                        </div>
+                        <div className={styles.seat_selected}>
+                            <div className={styles.seat_selected_title}>
+                                <div className={styles.seat_selected_title_left}>
+                                    선택 좌석
+                                </div>
+                                <div className={styles.seat_selected_title_right}>
+                                    총 {selectedSeats.length}석이 선택되었습니다.
+                                </div>
+                            </div>
+                            <div className={styles.seat_selected_main}>
+                                <div className={styles.seat_selected_header}>
+                                    <div className={styles.seat_selected_header_text1}>좌석등급</div>
+                                    <div className={styles.seat_selected_header_text2}>좌석번호</div>
+                                </div>
+                                <div className={styles.seat_selected_content}>
                                 {
-                                    sections.map((section, i)=>{
-                                        return(<button key={i} className={styles.section_btns} onClick={()=>{handleSection(section)}}>{section}</button>);
+                                    selectedSeats.map((seat)=>{
+                                        return(
+                                            <div className={styles.seat_selected_content_box}>
+                                                <div className={styles.seat_selected_content_text1}>level</div>
+                                                <div className={styles.seat_selected_content_text2}>{seat}</div>
+                                            </div>
+                                        );
                                     })
-                               }
+                                }
                                 </div>
                             </div>
-                            <div className={styles.seats_level}>
-                                <div className={styles.seats_level_text}> 좌석 등급 / 잔여석 </div>
-                                
-                                <div className={styles.seats_level_box}>
-                                {seatsLevel.map((level, i) => (
-                                    <div key={i} className={styles.level_box_data}>
-                                        <div className={styles.seats_level_color} style={{ backgroundColor: getBackgroundColor(level.level) }}></div>
-                                        {level.level}석 {level.price.toLocaleString()}원
-                                    </div>
-                                ))}
-                                </div>
-                                {/* 등급에 대해서는 고민필요
-                                    1. 만약 열에 대해 등급을 고정시킬 경우 (ex> 상영관 상관없이 1층 10행 열전체는 vip, 1층 나머지는 r)
-                                       db를 수정할 필요없이 랜더링 시 조건 부여해서 출력하면 될것같음. 
-                                    2. db에 등급을 넣을 경우 확실히 확장성이 늘어남, 근데 어떻게 db에 넣어야할지 감이 안잡힌다. 
-                                */}    
-                            </div>
-                            <div className={styles.seat_selected}>
-                                <div className={styles.seat_selected_title}>
-                                    <div className={styles.seat_selected_title_left}>
-                                        선택 좌석
-                                    </div>
-                                    <div className={styles.seat_selected_title_right}>
-                                        총 {selectedSeats.length}석이 선택되었습니다.
-                                    </div>
-                                </div>
-                                <div className={styles.seat_selected_main}>
-                                    <div className={styles.seat_selected_header}>
-                                        <div className={styles.seat_selected_header_text1}>좌석등급</div>
-                                        <div className={styles.seat_selected_header_text2}>좌석번호</div>
-                                    </div>
-                                    <div className={styles.seat_selected_content}>
-                                    {
-                                        selectedSeats.map((seat)=>{
-                                            return(
-                                                <div className={styles.seat_selected_content_box}>
-                                                    <div className={styles.seat_selected_content_text1}>level</div>
-                                                    <div className={styles.seat_selected_content_text2}>{seat}</div>
-                                                </div>
-                                            );
-                                        })
-                                    }
-                                    </div>
-                                </div>
-                            </div>
-                            <div className={styles.next_page}>
-                                <button style={{textAlign:"center"}} onClick={()=>{}}>다음페이지</button>
-                            </div>
+                        </div>
+                        <div className={styles.next_page}>
+                            <button style={{textAlign:"center"}} onClick={handlePriceModal}>다음페이지</button>
                         </div>
                     </div>
                 </div>
+                { priceModal && <PriceModal isOpen={priceModal} onClose={handleClosePriceModal} onBookClose={handleCloseBookModal}/>}
+            </div>
+            )}
+
+                {/* ui 구성하는 동안은 매크로 방지 제거 */}
+                
         </div>
     );
 
