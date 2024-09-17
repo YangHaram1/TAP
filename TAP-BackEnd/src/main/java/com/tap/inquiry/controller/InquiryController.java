b3406549be11a2a8cc8505dc01328626103d1e7b
YANGHARAM <apdlvmf33@naver.com>
2024-09-18 00:40:33 +0900

    update inquiry

--- TAP-BackEnd/src/main/java/com/tap/inquiry/controller/InquiryController.java
+++ TAP-BackEnd/src/main/java/com/tap/inquiry/controller/InquiryController.java
@@ -2,17 +2,22 @@ package com.tap.inquiry.controller;
 
 import java.security.Principal;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.tap.inquiry.dao.InquiryDAO;
 import com.tap.inquiry.dto.InquiryDTO;
+import com.tap.inquiry.service.InquiryService;
 
 @RestController
 @RequestMapping("inquiry")
 public class InquiryController {
+	@Autowired
+	private InquiryService serv;
 	
 	@PostMapping()
 	public ResponseEntity<String> insert(@RequestBody InquiryDTO dto,Principal principal) throws Exception {
@@ -21,7 +26,8 @@ public class InquiryController {
 			return ResponseEntity.ok(null);
 		}
 		String username = principal.getName();
-		
+		dto.setMember_id(username);
+		serv.insert(dto);
 		return ResponseEntity.ok(String.valueOf(dto.getSeq()));
 	}
 }
