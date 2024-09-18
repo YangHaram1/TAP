f40b8c2204263dac3d62f324092226b4e2c6a42a
Hayun Jeong <1116jjee@gmail.com>
2024-09-18 20:01:52 +0900

    popoup - application_seq에 맞는 공지사항 없을경우

--- TAP-FrontEnd/src/pages/Admin/Dash/Dash.jsx
+++ TAP-FrontEnd/src/pages/Admin/Dash/Dash.jsx
@@ -1,7 +1,31 @@
+import { useEffect } from 'react';
 import styles from './Dash.module.css'
+import { api } from '../../../config/config';
 
 export const Dash=()=>{
     
+    useEffect(()=>{
+        // 상품등록 건수 | created_at : 오늘, status: '승인 대기' 인 상품등록 건수 
+        // 상품세일 건수 | sale_approved : '승인 대기' 인 상품세일 건수 
+        // 배송 준비중 갯수 | delivery_status : '배송 준비중' 
+        api.get(`/admin/dash/getapply`)
+        api.get(`/admin/dash/getsale`)
+        api.get(`/admin/dash/getdelivery`)
+        
+        // 상품 등록 갯수 | updated_at : 오늘인 상품 갯수 
+        // 상품 세일 승인 갯수 | sale_approved : '승인 완료' 갯수
+        // 주문 - 배송 | delievery_status: '발송 완료' 갯수
+        api.get(`/admin/dash/getapplytoday`)
+        api.get(`/admin/dash/getsaletoday`)
+        api.get(`/admin/dash/getdeliverytoday`)
+        
+        // 주문 - 오늘 주문 | order_date : 오늘 + 총 금액
+        api.get(`/admin/dash/getorder`)
+        // 주문 | status : '환불' 인 건수 + 총 금액 
+        api.get(`/admin/dash/getrefund`)
+        // 카테고리 별 주문 건수  - group by 로 하면 카테고리 별 카운트 가져올 수 있나?
+        api.get(`admin/dash/getordercount`)
+    })
 
 
     return (
@@ -17,18 +41,18 @@ export const Dash=()=>{
                                 <tr>
                                     <td>상태</td>
                                     <td>건수</td>
-                                    <td>금액</td>
+                                    <td>비고</td>
                                 </tr>
                             </thead>
                             <tbody>
                                 <tr>
-                                    <td>결제완료</td>
+                                    <td>상품신청</td>
                                     <td> 건</td>
-                                    <td> 원</td>
+                                    <td> </td>
                                 </tr>
                                 <tr>
                                     <td>배송준비중</td>
-                                    <td></td>
+                                    <td>건</td>
                                     <td></td>
                                 </tr>
                                 <tr>
@@ -50,7 +74,7 @@ export const Dash=()=>{
                                 <tr>
                                     <td>상태</td>
                                     <td>건수</td>
-                                    <td>금액</td>
+                                    <td>비고</td>
                                 </tr>
                             </thead>
                             <tbody>
@@ -60,7 +84,7 @@ export const Dash=()=>{
                                     <td> 원</td>
                                 </tr>
                                 <tr>
-                                    <td>배송준비중</td>
+                                    <td>발송 완료</td>
                                     <td></td>
                                     <td></td>
                                 </tr>
